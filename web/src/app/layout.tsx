5ad1e86e47b949c9fd2fdbdbdd1c7b0e505c8da0
hashaam <hashaamustafa@gmail.com>
2025-08-26 10:41:40 +0100

    njh

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Sidebar } from "lucide-react";
 import { SidebarProvider, ThemeProvider } from "./dashboard/layout/page";
 
 const geistSans = Geist({
